c8331408ba2229cec74743ad480e0fb428342012
srigulli <sridharg_2005@yahoo.co.in>
2018-03-01 12:20:19 +0530

    NewServerJS

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/articleone', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
+});
+app.get('/articletwo', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
+});
+app.get('/articlethree', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -21,7 +29,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
