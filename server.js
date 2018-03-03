0a81d373e851e1c9fb06e7fc4a55ae217f6dc296
srigulli <sridharg_2005@yahoo.co.in>
2018-03-03 12:40:32 +0530

    varialbles

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName', function (req, res) {
-  var articleName = req.params.articleName;
+  var articleName = req.params.articlename;
   res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
